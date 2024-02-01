7e663473d262a161d0147dbbf778d7054b1b66d3
violetto-rose <manjumadhav.va@gmail.com>
2024-02-01 18:25:11 +0530

    modified:   OS Programs/Priority2.c modified:   OS Programs/RoundRobin2.c

--- OS Programs/Priority2.c
+++ OS Programs/Priority2.c
@@ -1,4 +1,5 @@
-#include <stdio.h> int main()
+#include <stdio.h>
+int main()
 {
     int bt[20], p[20], wt[20], tat[20], pr[20], i, j, n, total = 0, pos, temp, avg_wt, avg_tat;
     printf("Enter Total Number of Process:");
