27c14c80ebf2384439bcdc8bbc9449deb2c87ded
violetto-rose <manjumadhav.va@gmail.com>
2024-07-10 21:10:36 +0530

    modified:   ADA/8Subset.c

--- ADA/8Subset.c
+++ ADA/8Subset.c
@@ -1,30 +1,36 @@
-//Design and implement C Program to find a subset of a given set S = {sl , s2,.....,sn} of n positive integers whose sum is equal to a given positive integer d.
+// Design and implement C Program to find a subset of a given set S = {sl , s2,.....,sn} of n positive integers whose sum is equal to a given positive integer d.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 // Function to print a subset
-void printSubset(int subset[], int size) {
+void printSubset(int subset[], int size)
+{
     printf("Subset found: ");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", subset[i]);
     }
     printf("\n");
 }
 
 // Recursive function to find subsets
-bool findSubset(int set[], int subset[], int n, int subsetSize, int total, int targetSum, int index) {
-    if (total == targetSum) {
+bool findSubset(int set[], int subset[], int n, int subsetSize, int total, int targetSum, int index)
+{
+    if (total == targetSum)
+    {
         printSubset(subset, subsetSize);
         return true;
     }
-    if (index == n || total > targetSum) {
+    if (index == n || total > targetSum)
+    {
         return false;
     }
-    
+
     // Include the current element in the subset and move to the next element
     subset[subsetSize] = set[index];
-    if (findSubset(set, subset, n, subsetSize + 1, total + set[index], targetSum, index + 1)) {
+    if (findSubset(set, subset, n, subsetSize + 1, total + set[index], targetSum, index + 1))
+    {
         return true;
     }
 
@@ -32,19 +38,22 @@ bool findSubset(int set[], int subset[], int n, int subsetSize, int total, int t
     return findSubset(set, subset, n, subsetSize, total, targetSum, index + 1);
 }
 
-int main() {
+int main()
+{
     int n, d;
     printf("Enter the number of elements in the set: ");
     scanf("%d", &n);
 
     int *set = (int *)malloc(n * sizeof(int));
-    if (set == NULL) {
+    if (set == NULL)
+    {
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
 
     printf("Enter the elements of the set: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &set[i]);
     }
 
@@ -52,13 +61,15 @@ int main() {
     scanf("%d", &d);
 
     int *subset = (int *)malloc(n * sizeof(int));
-    if (subset == NULL) {
+    if (subset == NULL)
+    {
         fprintf(stderr, "Memory allocation failed\n");
         free(set);
         return 1;
     }
 
-    if (!findSubset(set, subset, n, 0, 0, d, 0)) {
+    if (!findSubset(set, subset, n, 0, 0, d, 0))
+    {
         printf("No subset found with the given sum.\n");
     }
 
